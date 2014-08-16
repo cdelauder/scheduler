0459a9a0fbb85d09c0419cb3416ca4178c88afc9
cdelauder <cdelauder@gmail.com>
2014-08-15 17:02:02 -0700

    added the rails-api gem to try to get around the demand for templates(thanks DHH :(). Have enough fuctionality available that I should be able to start creating objects using the testing app. However right now it is still pissy about the template issue. hopefully I can fix this soon

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
 
-  def timeslot
+  def timeslots
     new_timeslot = Timeslot.new(api_params)
     if new_timeslot.create
       status 200
@@ -9,8 +9,22 @@ class ApiController < ApplicationController
     end
   end
 
+  def timeslots
+    all_timeslots = Timeslot.all
+
+  end
+
+  def boat
+    new_boat = Boat.new(api_params)
+    if new_boat.create
+      status 200
+    else
+      status 500
+    end
+  end
+
   private
   def api_params
-    params.permit(:start_time, :duration)
+    params.permit(:start_time, :duration, :name, :capacity)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   resources :api, only: [] do
     collection do
-      post :timeslot
-      # get :timeslots
-      # post :boat
+      post :timeslots
+      get :timeslots
+      post :boat
       # get :boats
       # post :assignment
       # post :booking
