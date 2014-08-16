571db2e09fb829a8ed42df47032adcfdc1a45751
cdelauder <cdelauder@gmail.com>
2014-08-16 08:34:49 -0700

    finally figured out why things were broken. I missed a detail. applicationcontroller needs to inherit from api instead of base. now I need to make the objects actually be created, I missed something in my params handling

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,22 +1,26 @@
 class ApiController < ApplicationController
 
   def timeslots
+    all_timeslots = Timeslot.all
+    puts 'here?'
+  end
+
+  def timeslots
     new_timeslot = Timeslot.new(api_params)
+    puts new_timeslot
     if new_timeslot.create
+      puts 'creeated'
       respond_to do |format|
         format.json { render json: new_timeslot }
       end
     else
+      puts 'fail!'
       respond_to do |format|
         format.json { render json:  {message: 'Resource not found'} }
       end
     end
   end
 
-  def timeslots
-    all_timeslots = Timeslot.all
-
-  end
 
   def boat
     new_boat = Boat.new(api_params)
