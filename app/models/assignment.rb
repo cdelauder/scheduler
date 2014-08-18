01f3d8ce0275071ce1836863a0534582fd62ce2b
cdelauder <cdelauder@gmail.com>
2014-08-18 15:00:27 -0700

    just realized I left out something very important in my last method. I didnt check to see that sameday timeslots acutally overlap

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -5,6 +5,7 @@ class Assignment < ActiveRecord::Base
   before_save :update_availability
 
   def update_availability
+    # only update the availability if the newly assigned boat has a larger capacity than the current availability
     if self.timeslot.availability < self.boat.capacity
       self.timeslot.update_attributes(availability: self.boat.capacity)
     end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,10 +5,13 @@ class Booking < ActiveRecord::Base
   after_save :update_customer_count, :update_availability, :check_for_overlapping_timeslot
 
   def update_availability
+    # new booking will reduce availability by some amount
     self.timeslot.update_attributes(availability: determine_availability(self))
   end
 
   def determine_availability(booking)
+    # if there is only 1 boat in this timeslot availability goes down by the size of the booking
+    # if there is more than one availibility is whichever is greater between the availibility - size OR capacity of other boat
     if (booking.timeslot.availability - booking.size) > (booking.timeslot.boats.minimum(:capacity)) || booking.timeslot.boats.length ==1
       booking.timeslot.availability - booking.size
     else
@@ -17,22 +20,28 @@ class Booking < ActiveRecord::Base
   end
 
   def update_customer_count
+    # increase the customer count by the booking size
     self.timeslot.update_attributes(customer_count: (self.timeslot.customer_count += self.size))
   end
 
   def is_there_a_boat_with_enough_capacity
+    # if there is not a boat big enough to hold the party the booking can't be made
     if self.size > self.timeslot.boats.maximum(:capacity)
       errors.add('party is too large for the available boats')
     end
   end
 
   def is_there_availability_remaining
+    # There needs to be availability for that many people too
     if self.timeslot.availability < self.size
       errors.add('there is no longer enough availability remaining')
     end
   end
 
   def check_for_overlapping_timeslot
+    # check to see if there are other timeslots that day
+    # check to see if they use the same boats
+    # check to see if the timeslots actually overlap
     overlaps = Timeslot.where({start_time: (self.timeslot.start_time.at_beginning_of_day..self.timeslot.start_time.at_end_of_day)}).where.not(id: self.timeslot.id)
     overlaps.each do |overlap| 
       if overlap.boats == self.timeslot.boats && overlap.boats.length < 2
