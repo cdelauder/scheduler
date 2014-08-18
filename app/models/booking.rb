22ce2d97d7d6b15269e625a1c985b950ee7b4837
cdelauder <cdelauder@gmail.com>
2014-08-17 22:02:18 -0700

    I have gone down a long logic rabbit hole to no avail. I do not have the correct logic to reduce the availability of overlapping time slots. I need to walk away for a few now, because I am not seeing what is wrong.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,8 +10,10 @@ class Booking < ActiveRecord::Base
 
   def determine_availability(booking)
     if (booking.timeslot.availability - booking.size) > (booking.timeslot.boats.minimum(:capacity))
+      is_there_an_overlapping_timeslot(booking.timeslot.boats.order(:capacity).last)
       booking.timeslot.availability - booking.size
     else
+      is_there_an_overlapping_timeslot(booking.timeslot.boats.order(:capacity).first)
       booking.timeslot.boats.minimum(:capacity) 
     end
   end
@@ -29,6 +31,18 @@ class Booking < ActiveRecord::Base
   def is_there_availability_remaining
     if self.timeslot.availability < self.size
       errors.add('there is no longer enough availability remaining')
+    end
+  end
+
+  def is_there_an_overlapping_timeslot(boat)
+    overlaps = Timeslot.where({start_time: (self.timeslot.start_time.at_beginning_of_day..self.timeslot.start_time.at_end_of_day)})
+    overlaps.each do |overlap|
+      if boat.id = self.timeslot.boats.order(:capacity).last.id && self.id != overlap.id
+        end_time = self.timeslot.start_time + (self.timeslot.duration * 60)
+        if self.timeslot.start_time < overlap.start_time && end_time > overlap.start_time
+          overlap.update_attributes(availability: 0)
+        end
+      end
     end
   end
 end
